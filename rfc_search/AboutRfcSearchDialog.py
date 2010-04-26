669bb14447344c8bac33405650ed075c683c1106
Hemanth <hemanth.hm@gmail.com>
2010-04-26 12:02:17 +0530

    Initial project creation with Quickly!

---
+++ rfc_search/AboutRfcSearchDialog.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+### BEGIN LICENSE
+# This file is in the public domain
+### END LICENSE
+
+import gtk
+
+from rfc_search.helpers import get_builder
+
+import gettext
+from gettext import gettext as _
+gettext.textdomain('rfc-search')
+
+class AboutRfcSearchDialog(gtk.AboutDialog):
+    __gtype_name__ = "AboutRfcSearchDialog"
+
+    def __new__(cls):
+        """Special static method that's automatically called by Python when 
+        constructing a new instance of this class.
+        
+        Returns a fully instantiated AboutRfcSearchDialog object.
+        """
+        builder = get_builder('AboutRfcSearchDialog')
+        new_object = builder.get_object("about_rfc_search_dialog")
+        new_object.finish_initializing(builder)
+        return new_object
+
+    def finish_initializing(self, builder):
+        """Called while initializing this instance in __new__
+
+        finish_initalizing should be called after parsing the ui definition
+        and creating a AboutRfcSearchDialog object with it in order to
+        finish initializing the start of the new AboutRfcSearchDialog
+        instance.
+        
+        Put your initialization code in here and leave __init__ undefined.
+        """
+        # Get a reference to the builder and set up the signals.
+        self.builder = builder
+        self.builder.connect_signals(self)
+
+        # Code for other initialization actions should be added here.
+
+
+if __name__ == "__main__":
+    dialog = AboutRfcSearchDialog()
+    dialog.show()
+    gtk.main()
---
+++ setup.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+### BEGIN LICENSE
+# This file is in the public domain
+### END LICENSE
+
+###################### DO NOT TOUCH THIS (HEAD TO THE SECOND PART) ######################
+
+import os
+import sys
+
+try:
+    import DistUtilsExtra.auto
+except ImportError:
+    print >> sys.stderr, 'To build rfc-search you need https://launchpad.net/python-distutils-extra'
+    sys.exit(1)
+assert DistUtilsExtra.auto.__version__ >= '2.18', 'needs DistUtilsExtra.auto >= 2.18'
+
+def update_data_path(prefix, oldvalue=None):
+
+    try:
+        fin = file('rfc_search/rfc_searchconfig.py', 'r')
+        fout = file(fin.name + '.new', 'w')
+
+        for line in fin:            
+            fields = line.split(' = ') # Separate variable from value
+            if fields[0] == '__rfc_search_data_directory__':
+                # update to prefix, store oldvalue
+                if not oldvalue:
+                    oldvalue = fields[1]
+                    line = "%s = '%s'\n" % (fields[0], prefix)
+                else: # restore oldvalue
+                    line = "%s = %s" % (fields[0], oldvalue)
+            fout.write(line)
+
+        fout.flush()
+        fout.close()
+        fin.close()
+        os.rename(fout.name, fin.name)
+    except (OSError, IOError), e:
+        print ("ERROR: Can't find rfc_search/rfc_searchconfig.py")
+        sys.exit(1)
+    return oldvalue
+
+
+def update_desktop_file(datadir):
+
+    try:
+        fin = file('rfc-search.desktop.in', 'r')
+        fout = file(fin.name + '.new', 'w')
+
+        for line in fin:            
+            if 'Icon=' in line:
+                line = "Icon=%s\n" % (datadir + 'media/icon.png')
+            fout.write(line)
+        fout.flush()
+        fout.close()
+        fin.close()
+        os.rename(fout.name, fin.name)
+    except (OSError, IOError), e:
+        print ("ERROR: Can't find rfc-search.desktop.in")
+        sys.exit(1)
+
+
+class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
+    def run(self):
+        previous_value = update_data_path(self.prefix + '/share/rfc-search/')
+        update_desktop_file(self.prefix + '/share/rfc-search/')
+        DistUtilsExtra.auto.install_auto.run(self)
+        update_data_path(self.prefix, previous_value)
+
+
+        
+##################################################################################
+###################### YOU SHOULD MODIFY ONLY WHAT IS BELOW ######################
+##################################################################################
+
+DistUtilsExtra.auto.setup(
+    name='rfc-search',
+    version='0.1',
+    #license='GPL-3',
+    #author='Your Name',
+    #author_email='email@ubuntu.com',
+    #description='UI for managing …',
+    #long_description='Here a longer description',
+    #url='https://launchpad.net/rfc-search',
+    cmdclass={'install': InstallAndUpdateDataDirectory}
+    )
+
